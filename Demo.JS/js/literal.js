d2e5bf716893ad9cb184c78632e33c225fb7ceb1
N.kochnev <n.kochnev@erc.ur.ru>
2015-04-09 12:47:26 +0500

    Добавил проект Demo.JS

---
+++ Demo.JS/js/literal.js
@@ -0,0 +1,40 @@
+﻿var literal = {
+    init: function () {
+        $(function() {
+            literal.load();
+        });
+    },
+    load: function() {
+        alert();
+        var elem = literal.getElements();
+        literal.bindEvents(elem);
+        literal.createLi(elem);
+        literal.hideFirstLi(elem);
+    },
+    getElements: function () {
+        var list = $('#list');
+        var firstLi = $('#first');
+        var btn = $('#btn');
+        var elem = {
+            list: list,
+            firstLi: firstLi,
+            btn: btn
+        };
+        return elem;
+    },
+    bindEvents: function (elem) {
+        elem.btn.click(function() {
+            literal.showFirstLi(elem);
+        });
+    },
+    createLi: function (elem) {
+        var li = $('<li />').text('Новый элемент списка').css('color', 'red');
+        elem.list.append(li);
+    },
+    hideFirstLi: function (elem) {
+        elem.firstLi.hide();
+    },
+    showFirstLi: function(elem) {
+        elem.firstLi.show();
+    }
+};
---
+++ Demo.JS/js/module.js
@@ -0,0 +1,47 @@
+﻿var module = (function () {
+    // приватные переменная
+    var listSelectorName = "#list";
+    var firstSelectorName = "#first";
+    var btnSelectorName = "#btn";
+    //публичные переменные
+    return {
+        init: function() {
+            $(function() {
+                module.load();
+            });
+        },
+        load: function() {
+            alert();
+            var elem = module.getElements();
+            module.bindEvents(elem);
+            module.createLi(elem);
+            module.hideFirstLi(elem);
+        },
+        getElements: function() {
+            var list = $(listSelectorName);
+            var firstLi = $(firstSelectorName);
+            var btn = $(btnSelectorName);
+            var elem = {
+                list: list,
+                firstLi: firstLi,
+                btn: btn
+            };
+            return elem;
+        },
+        bindEvents: function(elem) {
+            elem.btn.click(function() {
+                module.showFirstLi(elem);
+            });
+        },
+        createLi: function(elem) {
+            var li = $('<li />').text('Новый элемент списка').css('color', 'red');
+            elem.list.append(li);
+        },
+        hideFirstLi: function(elem) {
+            elem.firstLi.hide();
+        },
+        showFirstLi: function(elem) {
+            elem.firstLi.show();
+        }
+    };
+}());
